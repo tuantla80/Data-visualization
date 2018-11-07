a5be20be680b1798cd35a73b5b228ce2553c105c
tuantla <tuantla80@gmail.com>
2018-11-07 20:05:34 +0900

    Initial commit

---
+++ Note on D3.js
@@ -0,0 +1,4 @@
+/* This file is to give some notes on D3.js so that it will reduce the time when developing a data visualization
+dashboard using D3.js.
+   For a full code of one particular example, please refer to https://bl.ocks.org/
+*/
