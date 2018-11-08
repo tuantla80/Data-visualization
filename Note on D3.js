f59afb3b9b3314f72303edb499e00bf040e92c8b
Tran Le Anh Tuan <39830700+tuantla80@users.noreply.github.com>
2018-11-09 00:07:26 +0900

    Update Note on D3.js

--- Note on D3.js
+++ Note on D3.js
@@ -3,7 +3,7 @@ dashboard using D3.js.
    For a full code of one particular example, please refer to https://bl.ocks.org/
 */
 
-// 1. Loading data *.json, *.csv, *.tsv,... Using map() function
+/* 1. Loading data *.json, *.csv, *.tsv,... Using map() function  */
 <script type="text/javascript">
     d3.csv("../../data/example.json",
         function (error, raw_data) {
@@ -17,3 +17,39 @@ dashboard using D3.js.
             });
         });
 </script>
+
+/* 2. Refer to "table.html"
+- Loading data: *.json, *.csv, *.tsv,... 
+- Using map() function: map for every object {...} in json file
+- Making simple html table
+*/
+
+/* 3 Refer to "svg_elements (circle, rect, ect).html"
+- Making svg elements.
+- Applying this simple element to make scatter plot, line plot and legend, ect.
+*/
+
+/* 4. Scaling
+- Refer to https://www.dashingd3js.com/d3js-scales
+- Domain values (our real data values) --> convert to --> Range values (pixel values)
+- 4 categories: continuous, ordinal (discrete), sequential (ex. range [0,1]), quantize (ex. each range for each bin color)
+*/
+// Example 1: How to set domain and range and how to show them side by side
+//   <div id='div1'></div>  <!-- make a div out side script -->
+   var data = d3.range(0, 11, 1); 
+   var xScale = d3.scaleLinear() //here v4 but at v3: d3.scale.linear()
+                  .domain([d3.min(data), d3.max(data)] // our real values
+                  .range([0, 800]);  // pixel values (display values)
+   d3.select('#div1').selectAll('div')
+      .data(data)
+      .enter()
+      .append('div') 
+      .text(function (d) {
+          return d + "    " + xScale(d); // d is real data and xScale(d) is a range values
+      });
+
+// Example 2: Scale for color of "data" at column "region" of a json file.
+   var regionColorScale = d3.scaleOrdinal()
+                            .domain(d3.set(data.map(function(d) { return d.region; })))
+                            .range(d3.schemeCategory20);
+
