dedaa41ebd89c3f45e31b751e3b79d3a440564b2
Tran Le Anh Tuan <39830700+tuantla80@users.noreply.github.com>
2018-11-09 00:22:31 +0900

    Update Note on D3.js

--- Note on D3.js
+++ Note on D3.js
@@ -52,4 +52,25 @@ dashboard using D3.js.
    var regionColorScale = d3.scaleOrdinal()
                             .domain(d3.set(data.map(function(d) { return d.region; })))
                             .range(d3.schemeCategory20);
+/* 5. Drawing Axes */
+// Example 1. x axis
+ var xAxis = d3.axisBottom()   // "Bottom mean the tick is at the bottom of axis line. Can choose axisTop()
+                .scale(xScale) // xScale is something like above
+                .ticks(20);    // Can ignore .ticks(20) so that D3 will automatically choose the number of ticks
+                               // NOTE: xAxis is just a function to generate svg for the axis. It still does NOT render the axis. 
+ svg.append('g') // 'g': grouping svg elements
+   .attr('transform', 'translate(0, ' + (height-30) + ')') // transate: x=0, y=(height-30) (in this case 30 is pixel) 
+   .call(xAxis); // Render the axis
+
+// Example 2. y axis
+var yAxis = d3.axisLeft()
+              .scale(yScale); // NOTE: (0,0) point is at the (top,left) so it will create a invert the axis.
+                               // To make it as normal.  
+                               //  var yScale = d3.scaleLinear()
+                               //                 .domain([d3.max(some_thing), 0]) // NOTE from max to 0
+                               //                 .range([number_1, number_2]);
+svg.append('g')
+   .attr('transform', 'translate(30,0)') // translate: x=30 (pixel) so that it will move to inside the canvas. y=0 (pixel)
+   .call(yAxis);
+
 
